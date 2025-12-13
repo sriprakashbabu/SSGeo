e6f7bdb139e8b4c1235c3f33419686d8a2ceb0a4
sri prakash <sriprakashbabu@gmail.com>
2025-12-14 01:55:05 +0530

    Started Grade 10 finshed 2 simulations

---
+++ SimplyScienceGeo/Assets/Grades/K10/TextBoxManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class TextBoxManager : MonoBehaviour
+{
+    public GameObject[] textBoxes;
+
+    private int currentIndex = -1;
+
+    public void ShowTextBox(int index)
+    {
+        // Deactivate currently active textbox
+        if (currentIndex >= 0 && currentIndex < textBoxes.Length)
+        {
+            textBoxes[currentIndex].SetActive(false);
+        }
+
+        // Activate new textbox
+        textBoxes[index].SetActive(true);
+        currentIndex = index;
+    }
+
+    // 🔴 RESET METHOD
+    public void ResetAll()
+    {
+        foreach (var box in textBoxes)
+        {
+            box.SetActive(false);
+        }
+
+        currentIndex = -1;
+    }
+}
